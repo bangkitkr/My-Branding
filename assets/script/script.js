13577f8b77461d3cf2f3ff745e82f5fe6cf58632
bangkitkr <102348274+bangkitkr@users.noreply.github.com>
2024-01-23 18:00:47 +0700

    Add files via upload

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,21 +6,25 @@ var type = new Typed(".textline", {
   loop: true,
 });
 
+// Animasi Navbar Ketika di Scroll
+const scrollNavbar = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  scrollNavbar.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// Burger Menu Ketika di Click
+const burger = document.querySelector("#hamburger-menu");
+const navLists = document.querySelector("nav .links");
+
+burger.addEventListener("click", () => {
+  navLists.classList.toggle("nav-active");
+  burger.classList.toggle("toggle-burger");
+});
+
 // Membersihkan Form Contact ketika Unload
 window.onbeforeunload = () => {
   for (const form of document.getElementsByTagName("form")) {
     form.reset();
   }
 };
-
-const navSlide = () => {
-  const burger = document.querySelector("#hamburger-menu");
-  const navLists = document.querySelector("nav .links");
-
-  burger.addEventListener("click", () => {
-    navLists.classList.toggle("nav-active");
-    burger.classList.toggle("toggle-burger");
-  });
-};
-
-navSlide();
